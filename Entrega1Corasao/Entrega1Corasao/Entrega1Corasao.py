5a203bf82bde7afa72c9b19fc0484be6638ca53f
MarPija <Mar@DESKTOP-S33GRMT>
2025-11-20 15:26:10 +0100

    Soc una Pija

--- Entrega1Corasao/Entrega1Corasao/Entrega1Corasao.py
+++ Entrega1Corasao/Entrega1Corasao/Entrega1Corasao.py
@@ -4,6 +4,7 @@ from traceback import print_tb
 print("sexe Ja siusplau: LITERAL")
 print("sexe Ja siusplau: LITERAL")
 print("sexe Ja siusplau: LITERAL")
+print("Q CHETOOOOO, Soc una pija de Sant Cugat (SOC LA MAR NO EL JAN)")
 
 
 print("no")
