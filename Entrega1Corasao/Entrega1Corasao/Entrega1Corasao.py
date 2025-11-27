9a40b0807c6cd7e2e33ce7577c2d136a5664d307
JanSolbes <jansolbesgodina@gmail.com>
2025-11-27 17:41:45 +0100

    Euler explícit a mitges

--- Entrega1Corasao/Entrega1Corasao/Entrega1Corasao.py
+++ Entrega1Corasao/Entrega1Corasao/Entrega1Corasao.py
@@ -1,10 +1,42 @@
-from traceback import print_tb
+import numpy as np
+
+K = 1
+Tc = 36.5
+dt = 1
+
+Xmax = 10
+tmax = 20
+dX = 1
+T01 = Tc
+T00 = Tc
+T0m1 = Tc
+Tj1 = [Tc]*(int(tmax/dt)+2)
+Ti = [Tc]*(int(Xmax/dX)+1)
+Tdef = np.zeros((int(Xmax/dX)+1,int(tmax/dt)+2))
+Tdef[0, :] = Tj1
+Tdef[:, 0] = Ti
 
 
-print("sexe Ja siusplau: LITERAL")
-print("sexe Ja siusplau: LITERAL")
-print("sexe Ja siusplau: LITERAL")
-print("Q CHETOOOOO, Soc una pija de Sant Cugat (SOC LA MAR NO EL JAN)")
+def tempEulExp(T0m1, T00, T01, dt, dX):
+    t = 0
+    j = 1
+    while (t <= tmax):
+        x = 0
+        i = 0
+        Tdef[:, int(tmax/dt)+1] = Tc
+        while (x < Xmax):
+                T01= Tdef[i+1,j]
+                T00 = Tdef[i, j]
+                T0m1 = Tdef[i-1, j]
+                T10 = dt*(((T01 - 2*T00 + T0m1)/(dX)**2) + 1) + T00
+                x = x + dX
+                i = i + 1
+                Tdef[i, j] = T10
+                Tdef[0,j] = Tc
+                
+        j = j + 1
+        t = t + dt
+    Tdef[:, int(tmax/dt)+1] = Tc
+    return Tdef
 
-
-print("no")
+print(tempEulExp(T0m1, T00, T01, dt, dX))
