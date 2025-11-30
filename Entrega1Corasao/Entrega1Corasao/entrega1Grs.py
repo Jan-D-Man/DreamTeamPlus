b20e7fcec0c55e45149bd26ab9de940c1ce78109
grise <grise@LAPTOP-JT77DTV1>
2025-11-30 17:55:35 +0100

    estic intentat veure lo del jan

--- Entrega1Corasao/Entrega1Corasao/entrega1Grs.py
+++ Entrega1Corasao/Entrega1Corasao/entrega1Grs.py
@@ -1,1 +1,10 @@
-print("Soc la Girs, cheto")
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+TC= 36.5
+
+#def T(dt):
+    #Tant = TC
+    #while(t<=0.025):
+        #T=Tant+dt*
+    #return y
