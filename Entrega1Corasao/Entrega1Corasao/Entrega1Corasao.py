e386ecb48c4a8ffb77ba3237130204a67c6a7d93
JanSolbes <jansolbesgodina@gmail.com>
2025-12-01 14:17:42 +0100

    Sexe: crec q ja està acabat euler explícit, cal canviar els valors de les constants i mirar si quadra

--- Entrega1Corasao/Entrega1Corasao/Entrega1Corasao.py
+++ Entrega1Corasao/Entrega1Corasao/Entrega1Corasao.py
@@ -2,41 +2,41 @@ import numpy as np
 
 K = 1
 Tc = 36.5
-dt = 1
+dt = 0.1
 
 Xmax = 10
 tmax = 20
-dX = 1
+dX = 0.1
 T01 = Tc
 T00 = Tc
 T0m1 = Tc
 Tj1 = [Tc]*(int(tmax/dt)+2)
-Ti = [Tc]*(int(Xmax/dX)+1)
-Tdef = np.zeros((int(Xmax/dX)+1,int(tmax/dt)+2))
-Tdef[0, :] = Tj1
-Tdef[:, 0] = Ti
+Ti = [Tc]*(int(Xmax/dX)+3)
+Tdef = np.zeros((int(tmax/dt)+2,int(Xmax/dX)+3))
+Tdef[:, 0] = Tj1
+Tdef[0, :] = Ti
 
 
 def tempEulExp(T0m1, T00, T01, dt, dX):
     t = 0
-    j = 1
+    j = 0
     while (t <= tmax):
         x = 0
-        i = 0
-        Tdef[:, int(tmax/dt)+1] = Tc
+        i = 1
+        Tdef[:, int(Xmax/dX)+2] = Tc
         while (x < Xmax):
-                T01= Tdef[i+1,j]
-                T00 = Tdef[i, j]
-                T0m1 = Tdef[i-1, j]
+                T01= Tdef[j,i+1]
+                T00 = Tdef[j, i]
+                T0m1 = Tdef[j, i-1]
                 T10 = dt*(((T01 - 2*T00 + T0m1)/(dX)**2) + 1) + T00
                 x = x + dX
                 i = i + 1
-                Tdef[i, j] = T10
-                Tdef[0,j] = Tc
+                Tdef[j+1, i-1] = T10
                 
         j = j + 1
         t = t + dt
-    Tdef[:, int(tmax/dt)+1] = Tc
+        Tdef[:, int(Xmax/dX)+2] = Tc
+        Tdef[:, 0] = Tc
     return Tdef
 
 print(tempEulExp(T0m1, T00, T01, dt, dX))
