02c4018bd3071ef0052a1abf66d6b4deb672e18f
grise <grise@LAPTOP-JT77DTV1>
2025-12-03 14:38:15 +0100

    sasaxsxsx

--- Entrega1Corasao/Entrega1Corasao/Entrega1Corasao.py
+++ Entrega1Corasao/Entrega1Corasao/Entrega1Corasao.py
@@ -1,3 +1,36 @@
+<<<<<<< HEAD
+﻿import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+K=0.56 
+c_v=3686
+ro=1081
+V=40
+sigma=0.472
+T_0=1
+
+t_adim=2*T_0*c_v*ro/(sigma*V**2)
+l_adim=K*t_adim/(c_v*ro)
+
+N_v = 100        # mida del vector (la matriu �s 100x100)
+Tc = 36.5   
+dX=0.02/l_adim
+dt = 0.5*dX**2
+gamma = dt/(dX)**2
+
+Xmax = 100
+tmax = 100
+
+T01 = Tc
+T00 = Tc
+T0m1 = Tc
+Tj1 = [Tc]*(int(tmax/dt)+2)
+Ti = [Tc]*(int(Xmax/dX)+3)
+Tdef = np.zeros((int(tmax/dt)+2,int(Xmax/dX)+3))
+Tdef[:, 0] = Tj1
+Tdef[0, :] = Ti
+=======
 ﻿# import numpy as np
 # import matplotlib.pyplot as plt
 # import math
@@ -28,6 +61,7 @@
 # Tdef = np.zeros((int(tmax/dt)+2,int(l_0/dX)+3))
 # Tdef[:, 0] = Tj1
 # Tdef[0, :] = Ti
+>>>>>>> origin/main
 
 
 # def tempEulExp(T0m1, T00, T01, dt, dX):
@@ -52,6 +86,27 @@
 #         Tdef[:, 0] = Tc
 #     return Tdef
 
+<<<<<<< HEAD
+def jacknicholson(T0m1, T00, T01,gamma):
+    diagonal   = (1 + 2*gamma) * np.ones(N_v) #diagonal -> np.ones ens construeix un vector de dimensi� N_v amb tot 1 
+    adalt  = (-gamma) * np.ones(N_v - 1) #�diagonal' de dalt i abaix 
+    abaix  = (-gamma) * np.ones(N_v - 1)
+
+    A = np.diag(diagonal) + np.diag(adalt, 1) + np.diag(abaix, -1) #constru�m A
+
+    diagonal   = (1 - 2*gamma) * np.ones(N_v) #diagonal -> np.ones ens construeix un vector de dimensi� N_v amb tot 1 
+    adalt  = (gamma) * np.ones(N_v - 1) #�diagonal' de dalt i abaix 
+    abaix  = (gamma) * np.ones(N_v - 1)
+
+    B = np.diag(diagonal) + np.diag(adalt, 1) + np.diag(abaix, -1) #constru�m B
+
+
+    pass
+
+
+
+print(jacknicholson(T0m1, T00, T01, gamma))
+=======
 
 
 
@@ -126,3 +181,4 @@ plt.show()
 
 # DeltaT = 0.5*DeltaX**2
 # alpha = DeltaT/(DeltaX)**2
+>>>>>>> origin/main
